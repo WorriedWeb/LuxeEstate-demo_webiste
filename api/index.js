5cbe967e4b295bc4b89677adce2c064ae8192e58
ankurkumar2002 <ankurkumarsingh0488@gmail.com>
2025-12-09 22:59:01 +0530

    changed runtime

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import serverless from "serverless-http";
 import app from "../server/index.js";
 
 export const config = {
-  runtime: "nodejs20.x",
+  runtime: "nodejs",
 };
 
 export default serverless(app);
